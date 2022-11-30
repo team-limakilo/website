60d772db12decf675a79e8ba13c715e73f553999
Ricardo Maes <ricmzn@gmail.com>
2022-11-29 21:50:38 -0300

    Add mobile fullscreen button

--- mobile.js
+++ mobile.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector(".menu");
 const shade = document.querySelector(".shade");
+const fullscreen = document.querySelector(".fullscreen");
 
 body.addEventListener("x-close-menu", () => {
     document.body.classList.remove("menu-open");
@@ -12,3 +13,15 @@ menu.addEventListener("click", () => {
 shade.addEventListener("click", () => {
     body.classList.remove("menu-open");
 });
+
+fullscreen.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+        body.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+});
+
+if (!document.fullscreenEnabled) {
+    fullscreen.remove();
+}
