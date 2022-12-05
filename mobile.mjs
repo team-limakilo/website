ec4ec5f1c9bbdba61422f7712af44c802d13ac91
Ricardo Maes <ricmzn@gmail.com>
2022-12-05 14:02:53 -0300

    Disable mobile menu on non-touch devices

--- mobile.mjs
+++ mobile.mjs
@@ -1,70 +1,68 @@
 import { addTouchOpenCloseHandler } from "./lib/touch.mjs";
 
-const body = document.body;
-const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const shade = document.querySelector(".shade");
-const menuBar = document.querySelector(".menu-bar");
+if (navigator.maxTouchPoints > 0) {
+    const body = document.body;
+    const nav = document.querySelector("nav");
+    const menu = document.querySelector(".menu");
+    const shade = document.querySelector(".shade");
+    const menuBar = document.querySelector(".menu-bar");
 
-/** @param {Event?} event */
-function closeMenu(event, force = false) {
-    if (force || selectedItem()) {
-        body.classList.remove("menu-open");
+    /** @param {Event?} event */
+    function closeMenu(event, force = false) {
+        if (force || selectedItem()) {
+            body.classList.remove("menu-open");
+            menuBar.classList.remove("hidden");
+            event?.preventDefault();
+            updateNavFocus();
+        }
+    }
+
+    /** @param {Event?} event */
+    function openMenu(event) {
+        body.classList.add("menu-open");
         menuBar.classList.remove("hidden");
         event?.preventDefault();
         updateNavFocus();
     }
-}
 
-/** @param {Event?} event */
-function openMenu(event) {
-    body.classList.add("menu-open");
-    menuBar.classList.remove("hidden");
-    event?.preventDefault();
-    updateNavFocus();
-}
-
-function selectedItem() {
-    return document.querySelector("nav .item.active");
-}
-
-function menuIsOpen() {
-    return body.classList.contains("menu-open");
-}
-
-function updateNavFocus() {
-    const tabIndex = menuIsOpen() ? "0" : "-1";
-    document.querySelectorAll("nav a").forEach((item) => {
-        item.tabIndex = tabIndex;
-        item.blur?.();
-    });
-}
-
-menu.addEventListener("click", openMenu);
-shade.addEventListener("click", closeMenu);
-body.addEventListener("x-close-menu", closeMenu);
-
-nav.addEventListener("transitionend", (event) => {
-    if (event.target === nav && event.propertyName === "left" && !menuIsOpen()) {
-        menuBar.classList.add("hidden");
+    function selectedItem() {
+        return document.querySelector("nav .item.active");
     }
-});
 
-addTouchOpenCloseHandler(body, nav, "close", true, () => menuIsOpen(), closeMenu);
-addTouchOpenCloseHandler(menu, nav, "open", false, () => !menuIsOpen(), openMenu);
-addTouchOpenCloseHandler(menuBar, nav, "open", false, () => !menuIsOpen(), openMenu);
+    function menuIsOpen() {
+        return body.classList.contains("menu-open");
+    }
 
-const mobileStyle = document.createElement("link");
-mobileStyle.href = "mobile.css";
-mobileStyle.rel = "stylesheet";
-body.appendChild(mobileStyle);
+    function updateNavFocus() {
+        const tabIndex = menuIsOpen() ? "0" : "-1";
+        document.querySelectorAll("nav a").forEach((item) => {
+            item.tabIndex = tabIndex;
+            item.blur?.();
+        });
+    }
 
-if (navigator.maxTouchPoints > 0) {
-    document.querySelector(".menu-bar").style.display = "block";
-}
+    menu.addEventListener("click", openMenu);
+    shade.addEventListener("click", closeMenu);
+    body.addEventListener("x-close-menu", closeMenu);
 
-if (selectedItem()) {
-    menuBar.classList.add("hidden");
-} else {
-    openMenu();
+    nav.addEventListener("transitionend", (event) => {
+        if (event.target === nav && event.propertyName === "left" && !menuIsOpen()) {
+            menuBar.classList.add("hidden");
+        }
+    });
+
+    addTouchOpenCloseHandler(body, nav, "close", true, () => menuIsOpen(), closeMenu);
+    addTouchOpenCloseHandler(menu, nav, "open", false, () => !menuIsOpen(), openMenu);
+    addTouchOpenCloseHandler(menuBar, nav, "open", false, () => !menuIsOpen(), openMenu);
+
+    const mobileStyle = document.createElement("link");
+    mobileStyle.href = "mobile.css";
+    mobileStyle.rel = "stylesheet";
+    body.appendChild(mobileStyle);
+
+    if (selectedItem()) {
+        menuBar.classList.add("hidden");
+    } else {
+        openMenu();
+    }
 }
