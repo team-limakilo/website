61142ad3c8afa7d01853d2660b275f480f9f5868
Ricardo Maes <ricmzn@gmail.com>
2023-07-31 22:31:26 +0100

    Add server system  specifications

--- servers/get_version.js
+++ servers/get_version.js
@@ -1,5 +1,5 @@
 const ver = document.getElementById("ver");
-ver.parentElement.classList.remove("hidden");
+ver.parentElement.classList.remove("version");
 fetch(ver.dataset.url)
     .then((response) => response.json())
     .then((data) => {
